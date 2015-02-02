f46320fa8d6ef5cf33b4f421f6dff29933b5f416
Mic <m@micz.it>
2015-02-02 23:52:29 +0100

    added onload function to statstab

---
+++ chrome/content/mzts-statstab.js
@@ -0,0 +1,13 @@
+"use strict";
+//Components.utils.import("chrome://thunderstats/lib/sqlite.js");
+//Components.utils.import("chrome://thunderstats/lib/tokenize.js");
+//Components.utils.import("chrome://thunderstats/lib/fileIO.js");
+
+var miczThunderStatsTab = {
+
+	onLoad: function(){
+		//TODO
+	},
+};
+
+window.addEventListener("load", miczThunderStatsTab.onLoad, false);

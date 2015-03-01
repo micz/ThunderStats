f6773f589fd29f6c60ea69456922b8d1adf22501
mic <m@micz.it>
2015-03-01 23:51:36 +0100

    tooltip function removed

--- chrome/content/mzts-statstab.ui.js
+++ chrome/content/mzts-statstab.ui.js
@@ -29,27 +29,4 @@ miczThunderStatsTab.ui={
 		window.openDialog('chrome://thunderstats/content/mzts-settings.xul','ThunderStats_Settings','non-private,chrome,titlebar,dialog=no,resizable,centerscreen').focus();
 	} ,
 
-	tooltip: function(aEvent, aShow) {
-		// Need to handle tooltips manually in xul-embedded-in-xhtml; tooltip
-		// element cannot be in the xhtml document either.
-		var tooltip = document.getElementById("mztsXulInXhtmlTooltip");
-		var tooltipdelay = parseInt(aEvent.target.getAttribute("tooltipdelay"));
-		tooltipdelay = isNaN(tooltipdelay) ? 800 : tooltipdelay;
-		if (aShow == 'show') {
-		  this.tiptimer = window.setTimeout(function() {
-							tooltip.label = aEvent.target.getAttribute("tooltiptext");
-							tooltip.openPopup(aEvent.target,
-											  "after_start",
-											  0, 0, false, false);
-						  }, tooltipdelay);
-		}
-		else if (aShow == 'hide') {
-		  window.clearTimeout(this.tiptimer);
-		  delete this.tiptimer;
-		  tooltip.label = "";
-		  tooltip.hidePopup();
-		  // Remove focus (for header button, arrow key binding issue).
-		  aEvent.target.blur();
-		}
-	},
 };

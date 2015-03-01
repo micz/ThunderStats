fbd42701252e56cff5aa2c2a5e77bb5418974697
mic <m@micz.it>
2015-03-01 23:33:34 +0100

    today top senders methods added

--- chrome/content/mzts-statstab.js
+++ chrome/content/mzts-statstab.js
@@ -47,6 +47,7 @@ var miczThunderStatsTab = {
 		miczThunderStatsTab.ui.showLoadingElement("today_sent_wait");
 		miczThunderStatsTab.ui.showLoadingElement("today_rcvd_wait");
 		miczThunderStatsTab.ui.showLoadingElement("today_recipients_wait");
+		miczThunderStatsTab.ui.showLoadingElement("today_senders_wait");
 
 		//Print dates
 		$jQ("#today_date").text(miczThunderStatsUtils.getTodayString());
@@ -58,11 +59,11 @@ var miczThunderStatsTab = {
 		//Get today received messages
 		miczThunderStatsCore.db.getTodayMessages(0,identity_id,miczThunderStatsTab.callback.homepage_stats_today_rcvd);
 
-		//Get today first 10 senders
-		//miczThunderStatsCore.db.getTodayInvolved(0,identity_id,miczThunderStatsTab.callback.homepage_stats_today_senders);
-
 		//Get today first 10 recipients
 		miczThunderStatsCore.db.getTodayInvolved(1,identity_id,miczThunderStatsTab.callback.homepage_stats_today_recipients);
+
+		//Get today first 10 senders
+		miczThunderStatsCore.db.getTodayInvolved(0,identity_id,miczThunderStatsTab.callback.homepage_stats_today_senders);
 	},
 
 	getYesterdayStats:function(identity_id){
@@ -273,6 +274,48 @@ miczThunderStatsTab.callback.homepage_stats_today_recipients = {
 		}
 	},
 };
+
+miczThunderStatsTab.callback.homepage_stats_today_senders = {
+	empty:true,
+	data:new Array(),
+  handleResult: function(aResultSet) {
+    let result = miczThunderStatsCore.db.getResultObject(["ID","Name","Mail","Num"],aResultSet);
+    this.empty=false;
+    for (let key in result) {
+		this.data.push(result[key]);
+	}
+	//dump('>>>>>>>>>>>>>> [miczThunderStatsTab] miczThunderStatsTab.callback.homepage_stats_today_senders handleResult '+JSON.stringify(this.data)+'\r\n');
+  },
+
+  handleError: miczThunderStatsTab.callback.base.handleError,
+
+  handleCompletion: function(aReason) {
+		switch (aReason) {
+			case Components.interfaces.mozIStorageStatementCallback.REASON_FINISHED:
+			    miczLogger.log("Today senders loaded.",0);
+				miczThunderStatsTab.ui.hideLoadingElement("today_senders_wait");
+				if(!this.empty){
+					$jQ("#today_senders").text(JSON.stringify(this.data));
+					//dump('>>>>>>>>>>>>>> [miczThunderStatsTab] miczThunderStatsTab.callback.homepage_stats_today_senders handleCompletion '+JSON.stringify(this.data)+'\r\n');
+				}else{
+					$jQ("#today_senders").text("No mails received!");
+				}
+				return true;
+			case Components.interfaces.mozIStorageStatementCallback.REASON_CANCELED:
+				miczLogger.log("Query canceled by the user!",1);
+				return false;
+			case Components.interfaces.mozIStorageStatementCallback.REASON_ERROR:
+				miczLogger.log("Query aborted!",2);
+				return false;
+		}
+	},
+};
+
+
+
+
+
+
 
 
 

90091aa488d30c24436184d779c3cde85a461f0c
mic <m@micz.it>
2015-03-01 23:22:54 +0100

    setup button with tooltip opens directly the pref window. fixes #8

--- chrome/content/mzts-statstab.ui.js
+++ chrome/content/mzts-statstab.ui.js
@@ -9,7 +9,7 @@ miczThunderStatsTab.ui={
 	hideLoadingElement:function(element){
 		$jQ("#"+element).hide();
 	},
-	
+
 	updateTab:function(tab_id){
 		switch(tab_id){
 			case '#tab_today':
@@ -23,5 +23,33 @@ miczThunderStatsTab.ui={
 				miczThunderStatsDB.close();
 			break;
 		}
-	}
+	},
+
+	openPrefWindow: function () {
+		window.openDialog('chrome://thunderstats/content/mzts-settings.xul','ThunderStats_Settings','non-private,chrome,titlebar,dialog=no,resizable,centerscreen').focus();
+	} ,
+
+	tooltip: function(aEvent, aShow) {
+		// Need to handle tooltips manually in xul-embedded-in-xhtml; tooltip
+		// element cannot be in the xhtml document either.
+		var tooltip = document.getElementById("mztsXulInXhtmlTooltip");
+		var tooltipdelay = parseInt(aEvent.target.getAttribute("tooltipdelay"));
+		tooltipdelay = isNaN(tooltipdelay) ? 800 : tooltipdelay;
+		if (aShow == 'show') {
+		  this.tiptimer = window.setTimeout(function() {
+							tooltip.label = aEvent.target.getAttribute("tooltiptext");
+							tooltip.openPopup(aEvent.target,
+											  "after_start",
+											  0, 0, false, false);
+						  }, tooltipdelay);
+		}
+		else if (aShow == 'hide') {
+		  window.clearTimeout(this.tiptimer);
+		  delete this.tiptimer;
+		  tooltip.label = "";
+		  tooltip.hidePopup();
+		  // Remove focus (for header button, arrow key binding issue).
+		  aEvent.target.blur();
+		}
+	},
 };

331604033393061a1f16071fc4446bb183ca9863
mic <m@micz.it>
2015-03-20 23:04:55 +0100

    inbox 0 folder spread graph improved. fixes #36

--- chrome/content/mzts-statstab.ui.js
+++ chrome/content/mzts-statstab.ui.js
@@ -337,7 +337,164 @@ miczThunderStatsTab.ui={
 	},
 
 	drawInbox0FolderSpreadGraph:function(element_id_txt,data_array){
-		let width = 250,
+
+		//remove old graph
+		$jQ("#"+element_id_txt+"_svg_graph").remove();
+
+		let svg = d3.select("#"+element_id_txt)
+			.append("svg")
+			.attr("id",element_id_txt+"_svg_graph")
+			.append("g");
+
+		svg.append("g")
+			.attr("class", "slices");
+		svg.append("g")
+			.attr("class", "labels");
+		svg.append("g")
+			.attr("class", "lines");
+
+		let width = 350,
+			height = 200,
+			radius = Math.min(width, height) / 2;
+
+		//calculating total elements
+		let data_sum=0;
+		for(let key in data_array){
+			data_sum+=data_array[key].Num;
+		}
+
+		//normalize data
+		let norm_data=new Array();
+		for(let key in data_array){
+			let element={};
+			element.value=data_array[key].Num;
+			element.label=data_array[key].Folder;
+			element.normalized=data_array[key].Num/data_sum;
+			norm_data.push(element);
+		}
+
+		let pie = d3.layout.pie()
+			.sort(null)
+			.value(function(d) {
+				return d.value;
+			});
+
+		let arc = d3.svg.arc()
+			.outerRadius(radius * 0.8)
+			.innerRadius(radius * 0.4);
+
+		let outerArc = d3.svg.arc()
+			.innerRadius(radius * 0.9)
+			.outerRadius(radius * 0.9);
+
+		svg.attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
+
+		let key = function(d){ return d.data.label; };
+
+		let color = d3.scale.category20();
+
+			/* ------- PIE SLICES -------*/
+			let slice = svg.select(".slices").selectAll("path.slice")
+				.data(pie(norm_data), key);
+
+			slice.enter()
+				.insert("path")
+				.style("fill", function(d) { return color(d.data.label); })
+				.attr("class", "slice");
+
+			slice
+				.transition().duration(1000)
+				.attrTween("d", function(d) {
+					this._current = this._current || d;
+					let interpolate = d3.interpolate(this._current, d);
+					this._current = interpolate(0);
+					return function(t) {
+						return arc(interpolate(t));
+					};
+				})
+
+			slice.exit()
+				.remove();
+
+			/* ------- TEXT LABELS -------*/
+
+			let text = svg.select(".labels").selectAll("text")
+				.data(pie(norm_data), key);
+
+			text.enter()
+				.append("text")
+				.attr("dy", ".35em")
+				.text(function(d) {
+					return d.data.label;
+				})
+				.attr("class","tooltip")
+				.attr("title",function(d){ return "Mails: "+d.data.value+" ("+(d.data.normalized*100).toFixed(0)+"%)";});
+
+			  $jQ('text.tooltip').tooltipster({debug:false,theme:'tooltipster-light',contentAsHTML:true,arrow:false});
+
+			function midAngle(d){
+				return d.startAngle + (d.endAngle - d.startAngle)/2;
+			}
+
+			text.transition().duration(1000)
+				.attrTween("transform", function(d) {
+					this._current = this._current || d;
+					let interpolate = d3.interpolate(this._current, d);
+					this._current = interpolate(0);
+					return function(t) {
+						let d2 = interpolate(t);
+						let pos = outerArc.centroid(d2);
+						pos[0] = radius * (midAngle(d2) < Math.PI ? 1 : -1);
+						return "translate("+ pos +")";
+					};
+				})
+				.styleTween("text-anchor", function(d){
+					this._current = this._current || d;
+					let interpolate = d3.interpolate(this._current, d);
+					this._current = interpolate(0);
+					return function(t) {
+						let d2 = interpolate(t);
+						return midAngle(d2) < Math.PI ? "start":"end";
+					};
+				});
+
+			text.exit()
+				.remove();
+
+			/* ------- SLICE TO TEXT POLYLINES -------*/
+
+			var polyline = svg.select(".lines").selectAll("polyline")
+				.data(pie(norm_data), key);
+
+			polyline.enter()
+				.append("polyline");
+
+			polyline.transition().duration(1000)
+				.attrTween("points", function(d){
+					this._current = this._current || d;
+					let interpolate = d3.interpolate(this._current, d);
+					this._current = interpolate(0);
+					return function(t) {
+						let d2 = interpolate(t);
+						let pos = outerArc.centroid(d2);
+						pos[0] = radius * 0.95 * (midAngle(d2) < Math.PI ? 1 : -1);
+						return [arc.centroid(d2), outerArc.centroid(d2), pos];
+					};
+				});
+
+			polyline.exit()
+				.remove();
+
+
+
+
+
+
+
+
+
+
+		/*let width = 250,
 			height = 250,
 			radius = Math.min(width, height) / 2;
 

f4da702f7cc067eb2278e1107adefe97da829228
mic <m@micz.it>
2015-06-05 22:46:18 +0200

    added an opntion to force inbox mails search only in folders for the selected account

--- defaults/preferences/prefs.js
+++ defaults/preferences/prefs.js
@@ -1,3 +1,4 @@
 pref("extensions.ThunderStats.debug", false);
 pref("extensions.ThunderStats.identities_selector", false);
 pref("extensions.ThunderStats.strt_acc", "0");
+pref("extensions.ThunderStats.inbox_search_only_curr_account", true);

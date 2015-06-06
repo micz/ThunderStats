dafe79de9518fdd8e34c268c2a58c7debf69a05d
mic <m@micz.it>
2015-06-06 20:05:18 +0200

    global update option added

--- defaults/preferences/prefs.js
+++ defaults/preferences/prefs.js
@@ -2,3 +2,4 @@ pref("extensions.ThunderStats.debug", false);
 pref("extensions.ThunderStats.identities_selector", false);
 pref("extensions.ThunderStats.strt_acc", "0");
 pref("extensions.ThunderStats.inbox_search_only_curr_account", true);
+pref("extensions.ThunderStats.global_update", false);

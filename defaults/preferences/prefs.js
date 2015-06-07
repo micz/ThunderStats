3b94897b02564e2f5037255bc19797e670286ab8
mic <m@micz.it>
2015-06-07 23:38:05 +0200

    added an option to show the yesterday mails in the today time graph. see #70

--- defaults/preferences/prefs.js
+++ defaults/preferences/prefs.js
@@ -3,3 +3,4 @@ pref("extensions.ThunderStats.identities_selector", false);
 pref("extensions.ThunderStats.strt_acc", "0");
 pref("extensions.ThunderStats.inbox_search_only_curr_account", true);
 pref("extensions.ThunderStats.global_update", true);
+pref("extensions.ThunderStats.today_time_graph_show_yesterday", true);

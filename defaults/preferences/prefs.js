b6151af8f78beb7da482dc4ced0066f3dea60942
mic <m@micz.it>
2015-06-14 11:18:09 +0200

    progressive bars in time graph option added. see #69

--- defaults/preferences/prefs.js
+++ defaults/preferences/prefs.js
@@ -4,3 +4,4 @@ pref("extensions.ThunderStats.strt_acc", "0");
 pref("extensions.ThunderStats.inbox_search_only_curr_account", true);
 pref("extensions.ThunderStats.global_update", true);
 pref("extensions.ThunderStats.today_time_graph_show_yesterday", true);
+pref("extensions.ThunderStats.today_time_graph_progressive", false);

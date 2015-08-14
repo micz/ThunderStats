1da688a3c52c71cff2884c8b0351b9074d20bfb2
Mic <m@micz.it>
2015-08-14 12:12:55 +0200

    working on the js promise. see #96

--- chrome/content/mzts-statstab.promises.js
+++ chrome/content/mzts-statstab.promises.js
@@ -5,5 +5,23 @@ Components.utils.import("resource://thunderstats/miczLogger.jsm");
 miczThunderStatsTab.promises={};
 
 miczThunderStatsTab.promises.customqry = {
+	
+	_current_analyzer:null,
+	
+	setAnalyzer:function(){
+		this._current_analyzer=new Promise(
+			function(resolve, reject) {
+				//TO DO
+			});
+		this._current_analyzer.then(
+			function(val){
+				
+			})
+		.catch(
+			function(reason) {
+				miczLogger.log('Handle rejected promise ('+reason+') here.');
+			});
+
+	},
 
 };

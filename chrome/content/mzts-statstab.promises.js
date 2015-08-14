98d6b6b770b25835560f34afab74dfc9aadad37c
Mic <m@micz.it>
2015-08-14 12:02:30 +0200

    adding js promises. see #96

---
+++ chrome/content/mzts-statstab.promises.js
@@ -0,0 +1,9 @@
+"use strict";
+Components.utils.import("chrome://thunderstats/content/mzts-utils.jsm");
+Components.utils.import("resource://thunderstats/miczLogger.jsm");
+
+miczThunderStatsTab.promises={};
+
+miczThunderStatsTab.promises.customqry = {
+
+};

48f1241f0698b0989644403e28386d0c938a3fdf
Mic <m@micz.it>
2015-08-15 23:35:21 +0200

    customqry: setanalyzer method implemented for aggregate data. see #96

--- chrome/content/mzts-statstab.promises.js
+++ chrome/content/mzts-statstab.promises.js
@@ -7,14 +7,32 @@ miczThunderStatsTab.promises={};
 miczThunderStatsTab.promises.customqry = {
 	
 	_current_analyzer:null,
+	_analyzer_data:{},
 	
-	setAnalyzer:function(){
+	setAnalyzer:function(type,data){
 		this._current_analyzer=new Promise(
 			function(resolve, reject){
-				//TO DO
+				this._analyzer_data[type]={};
+				this._analyzer_data[type]['max']=0;
+				this._analyzer_data[type]['min']=0;
+				this._analyzer_data[type]['avg']=0;
+				// data structure, from mzts-statstab.ui.js
+				// {day:m.unix(),day_str:miczThunderStatsUtils.getDateStringYY(m,true),num:this.data[1]["Num"]}
+				// do aggregate functions (max, min, avg)
+				for (let ai in data){
+					this._analyzer_data[type]['max']=data[ai]['num']>this._analyzer_data[type]['max']:data[ai]['num']?this._analyzer_data[type]['max'];
+					this._analyzer_data[type]['min']=data[ai]['num']<this._analyzer_data[type]['min']:data[ai]['num']?this._analyzer_data[type]['min'];
+					this._analyzer_data[type]['avg']+=data[ai]['num'];
+				}
+				this._analyzer_data[type]['avg']=this._analyzer_data[type]['avg']/data.length;
+				resolve(type);
 			});
-			
 		},
+		
+	resetAnalyzer:function(){
+		this._current_analyzer=null;
+		this._analyzer_data={};
+	},
 	
 	doAnalyzer:function(){
 		this._current_analyzer.then(
@@ -23,7 +41,7 @@ miczThunderStatsTab.promises.customqry = {
 			})
 		.catch(
 			function(reason){
-				miczLogger.log('[miczThunderStatsTab.promises.customqry] Handle rejected promise ('+reason+') here.');
+				miczLogger.log('[miczThunderStatsTab.promises.customqry] Handle rejected promise ('+reason+') here. Type: '+type+'.');
 			});
 
 	},

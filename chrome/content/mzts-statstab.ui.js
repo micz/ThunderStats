d45d3dc5ff0dc5428bbf47355f60bccd0a885f89
mic <m@micz.it>
2015-08-27 23:42:41 +0200

    correctly showing the folder and the last message for today / yesterday. see #95

--- chrome/content/mzts-statstab.ui.js
+++ chrome/content/mzts-statstab.ui.js
@@ -570,6 +570,7 @@ miczThunderStatsTab.ui={
 			element.value=data_array[key].Num;
 			element.label=data_array[key].Folder;
 			element.folder_url=data_array[key].FolderURI;
+			element.mKey=data_array[key].mKey;
 			element.normalized=data_array[key].Num/data_sum;
 			norm_data.push(element);
 		}
@@ -651,7 +652,11 @@ miczThunderStatsTab.ui={
 									}else{
 										let tabmail = mdoc.getElementById("tabmail");
 										tabmail.selectTabByIndex(null,0);
+										dump(">>>>>>>>>>>>>> [miczThunderStatsTab drawInbox0FolderSpreadGraph] mKey: "+JSON.stringify(d.data.mKey)+"\r\n");
 										mwin.gFolderTreeView.selectFolder(MailUtils.getFolderForURI(d.data.folder_url));
+										try{
+											mwin.gFolderDisplay.selectMessage(mwin.gFolderDisplay.displayedFolder.GetMessageHeader(d.data.mKey));
+										}catch(e){}
 									}
 								})
 				.attr("title",function(d){ return d.data.label+"<br/>"+_bundleCW.GetStringFromName("ThunderStats.Mails")+": "+d.data.value+" ("+(d.data.normalized*100).toFixed(0)+"%)";});

0817bcc340780fbe78c9baf0fc1babaf4506ccc2
mic <m@micz.it>
2016-11-12 15:54:37 +0100

    first step added

--- chrome/content/mzts-settings-debugger.js
+++ chrome/content/mzts-settings-debugger.js
@@ -1,5 +1,10 @@
 "use strict";
 Components.utils.import("chrome://thunderstats/content/mzts-utils.jsm");
+Components.utils.import("chrome://thunderstats/content/dbutils/mzts-mdb.jsm");
+Components.utils.import("chrome://thunderstats/content/mzts-statscore.jsm");
+Components.utils.import("resource://thunderstats/miczLogger.jsm");
+Components.utils.import("chrome://thunderstats/content/mzts-statstab.i18n.jsm");
+Components.utils.import("chrome://thunderstats/content/mzts-statstab.prefs.jsm");
 
 var miczThunderStatsDebugger = {
 
@@ -7,6 +12,7 @@ var miczThunderStatsDebugger = {
 		//Fixing window height
 		this.fixWinHeight();
 
+		moment.locale(miczThunderStatsUtils.getCurrentSystemLocale());
 		this.run();
 	},
 
@@ -25,16 +31,22 @@ var miczThunderStatsDebugger = {
 		let today = new Date();
 		this.addLogLines('['+today+']');
 		this.addLogLines(' ');
+		this.addLogLines('--------------------------------------');
 		this.addLogLines('-==          System Info           ==-');
+		this.addLogLines('--------------------------------------');
 		this.addLogLines(this.getSystemInfo());
 		this.addLogLines('-====================================-');
 		this.addLogLines(' ');
+		this.addLogLines('--------------------------------------');
 		this.addLogLines('-==   Thunderbird Configuration    ==-');
-		this.addLogLines(this.getTBConf());
+		this.addLogLines('--------------------------------------');
+		this.addLogLines(this.getThunderbirdConf());
 		this.addLogLines('-====================================-');
 		this.addLogLines(' ');
-		this.addLogLines('-====================================-');
-		this.addLogLines('-====================================-');
+		this.addLogLines('--------------------------------------');
+		this.addLogLines('-==       ThunderStats Debug       ==-');
+		this.addLogLines('--------------------------------------');
+		this.getThunderStatsData(0);
 	},
 
 	getSystemInfo:function(){
@@ -46,10 +58,30 @@ var miczThunderStatsDebugger = {
 		return output.trim();
 	},
 
-	getTBConf:function(){
+	getThunderbirdConf:function(){
 		let output='';
 		output+="Global Indexing: "+(miczThunderStatsUtils.checkGlobalIndexing?'enabled':'disabled')+"\r\n";
 		return output.trim();
+	},
+
+	getThunderStatsData:function(step){
+		switch(step){
+			case 0:	//get last indexed message
+				miczThunderStatsDB.init();
+				miczThunderStatsDB.queryGetLastMessageDate(miczThunderStatsDebugger.callback.last_idx_msg);
+			break;
+			case 1:	//get last index update
+				//miczThunderStatsDB.getLastIndexUpdate();
+				miczThunderStatsDebugger.getThunderStatsData(10);
+			break;
+			case 10:	//last step
+				miczThunderStatsDB.close();
+				miczThunderStatsDebugger.addLogLines('-====================================-');
+				miczThunderStatsDebugger.addLogLines(' ');
+				miczThunderStatsDebugger.addLogLines('-====================================-');
+				miczThunderStatsDebugger.addLogLines('-====================================-');
+			break;
+		}
 	},
 
 	addLogLines:function(text){
@@ -64,3 +96,70 @@ var miczThunderStatsDebugger = {
 
 };
 
+
+//callbacks
+miczThunderStatsDebugger.callback={};
+
+miczThunderStatsDebugger.callback.base={
+
+	handleError: function(aError) {
+		miczLogger.log("Error in executeAsync: " + aError.message,2);
+	},
+
+	handleCompletion: function(aReason) {
+		switch (aReason) {
+			case Components.interfaces.mozIStorageStatementCallback.REASON_FINISHED:
+				//miczLogger.log("Query completed successfully.");
+				return true;
+			case Components.interfaces.mozIStorageStatementCallback.REASON_CANCELED:
+				miczLogger.log("Query canceled by the user!",1);
+				return false;
+			case Components.interfaces.mozIStorageStatementCallback.REASON_ERROR:
+				miczLogger.log("Query aborted!",2);
+				return false;
+		}
+	return false;
+	},
+};
+
+miczThunderStatsDebugger.callback.last_idx_msg = {
+	empty:true,
+	data:{},
+	handleResult: function(aResultSet) {
+		this.empty=false;
+		let result = miczThunderStatsCore.db.getResultObject(["last_msg_date"],aResultSet);
+		for (let key in result) {
+			this.data[key]=result[key];
+		}
+	},
+
+  handleError: miczThunderStatsDebugger.callback.base.handleError,
+
+  handleCompletion: function(aReason) {
+		switch (aReason) {
+			case Components.interfaces.mozIStorageStatementCallback.REASON_FINISHED:
+				if(!this.empty){
+					let _bundleCW = miczThunderStatsI18n.createBundle("mzts-statstab");
+					miczThunderStatsDebugger.addLogLines(_bundleCW.GetStringFromName("ThunderStats.LastIndexedMessage")+": "+miczThunderStatsUtils.getDateTimeString(moment(this.data[1]["last_msg_date"]/1000)));
+				}else{
+					miczThunderStatsDebugger.addLogLines(_bundleCW.GetStringFromName("ThunderStats.LastIndexedMessage")+": not found.");
+				}
+				miczLogger.log("Last indexed message loaded.",0);
+				this.data={};
+				this.empty=true;
+				miczThunderStatsDebugger.getThunderStatsData(1);
+				return true;
+			case Components.interfaces.mozIStorageStatementCallback.REASON_CANCELED:
+				miczLogger.log("Query canceled by the user!",1);
+				this.data={};
+				this.empty=true;
+				return false;
+			case Components.interfaces.mozIStorageStatementCallback.REASON_ERROR:
+				miczLogger.log("Query aborted!",2);
+				this.data={};
+				this.empty=true;
+				return false;
+		}
+	return false;
+	},
+};

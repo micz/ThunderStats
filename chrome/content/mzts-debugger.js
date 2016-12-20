8f016f72d17f4a4b7c355d522b7fe8aae5ee9b8f
mic <m@micz.it>
2016-12-20 23:39:21 +0100

    debugger: added mandatory user message. fixes #132

--- chrome/content/mzts-debugger.js
+++ chrome/content/mzts-debugger.js
@@ -69,8 +69,27 @@ var miczThunderStatsDebugger = {
 	},
 
 	sendReport:function(){
+		if(document.getElementById('mzts-debugger-user-msg').value==''){
+			document.getElementById('mzts-debugger-user-msg').style.border='2 px solid red';
+			document.getElementById('mzts_user_msg_label').style.color='red';
+			document.getElementById('mzts_user_msg_label').style.fontWeight='bold';
+			return;
+		}
+		let message_body=" ";
+		message_body+="\r\n";
+		message_body+='-====================================-';
+		message_body+="\r\n";
+		message_body+='-== ThunderStats Debugger User Message ==-';
+		message_body+="\r\n";
+		message_body+='-====================================-';
+		message_body+="\r\n";
+		message_body+=document.getElementById('mzts-debugger-user-msg').value;
+		message_body+="\r\n";
+		message_body+='-====================================-';
+		message_body+="\r\n\r\n";
+		message_body+=document.getElementById('mzts-debugger-log').value;
 		let prompts = Components.classes["@mozilla.org/embedcomp/prompt-service;1"].getService(Components.interfaces.nsIPromptService);
-	    let sURL="mailto:" + miczThunderStatsUtils.mailto + "?subject=[ThunderStats] Debugger Report&body=" + encodeURI(" " + document.getElementById('mzts-debugger-log').value); // urlencode
+	    let sURL="mailto:" + miczThunderStatsUtils.mailto + "?subject=[ThunderStats] Debugger Report&body=" + encodeURI(message_body); // urlencode
 		let MessageComposer=Components.classes["@mozilla.org/messengercompose;1"].getService(Components.interfaces.nsIMsgComposeService);
 		let ioService = Components.classes["@mozilla.org/network/io-service;1"].getService(Components.interfaces.nsIIOService);
 		let aURI = ioService.newURI(sURL, null, null);

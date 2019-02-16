7c2368c92be992cf1d93aff963107c326a42cfa2
Christopher Leidigh <cleidigh@Gmail.com>
2019-02-16 00:48:36 -0500

    really fix script

--- scripts/build-xpi.bat
+++ scripts/build-xpi.bat
@@ -1,3 +1,3 @@
 call jpm xpi --dest-dir ./xpi --addon-dir ./src 
-call 7z d .\xpi\columns-wizard-4.2.2-beta1-tb.xpi bootstrap.js
-call 7z a .\xpi\thunderstats-4.2.2-beta1-tb.xpi ./src/manifest.json
+call 7z d .\xpi\thunderstats-1.4.3-beta1-tb.xpi bootstrap.js
+call 7z a .\xpi\thunderstats-1.4.3-beta1-tb.xpi ./src/manifest.json

bc4753081ee1490a991d8697ece00af5ededc723
Christopher Leidigh <cleidigh@Gmail.com>
2019-02-16 00:45:52 -0500

    Fix build script

--- scripts/build-xpi.bat
+++ scripts/build-xpi.bat
@@ -1,3 +1,3 @@
 call jpm xpi --dest-dir ./xpi --addon-dir ./src 
-call 7z d .\xpi\columns-wizard-6.1.1-beta1-tb.xpi bootstrap.js
-call 7z a .\xpi\columns-wizard-6.1.1-beta1-tb.xpi ./src/manifest.json
+call 7z d .\xpi\columns-wizard-4.2.2-beta1-tb.xpi bootstrap.js
+call 7z a .\xpi\thunderstats-4.2.2-beta1-tb.xpi ./src/manifest.json

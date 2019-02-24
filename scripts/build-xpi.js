9d3a47a84f463cdb6338997551f28589e2af7d97
Christopher Leidigh <cleidigh@Gmail.com>
2019-02-24 00:01:41 -0500

    Update build scripts - usable pass

--- scripts/build-xpi.js
+++ scripts/build-xpi.js
@@ -1,21 +1,24 @@
-// cleidigh
+// cleidigh - build Thunderbird add-on - Use package configuration for XUL or hybrid with manifest
 
-let fs = require('fs');
+const fs = require('fs');
 const _7z = require('7zip-min');
+const xml_util = require('./xml-util.js');
+const loadJsonFile = require('load-json-file');
 
 // Configuration args
 
-let targetBaseName = process.env.npm_package_name;
-let targetVersion = process.env.npm_package_version;
-let targetSuffix = process.env.npm_package_config_target_suffix;
-let targetExtension = process.env.npm_package_config_target_extension;
-
-let sourceDir = process.env.npm_package_config_source_dir;
-let targetDir = process.env.npm_package_config_target_dir;
-
 let targetName = '';
 
-// console.log(process.env);
+const targetBaseName = process.env.npm_package_name;
+const targetVersion = process.env.npm_package_version;
+const targetSuffix = process.env.npm_package_config_target_suffix;
+const targetExtension = process.env.npm_package_config_target_extension;
+const includeManifest = (process.env.npm_package_config_target_include_manifest === 'true') ? true : false;
+
+const sourceDir = process.env.npm_package_config_source_dir;
+const targetDir = process.env.npm_package_config_target_dir;
+
+// Validate configuration properties
 
 try {
 	if (typeof targetBaseName !== 'string') {
@@ -37,23 +40,55 @@ try {
 		targetExtension = '.xpi';
 	}
 
-
 	targetName = `${targetBaseName}-${targetVersion}${targetSuffix}${targetExtension}`;
 } catch (error) {
-	console.error(error);
+	console.error('Build Error: ' + error);
 	return 1;
 }
 
-console.log(targetName);
+console.log('Building Target::\n');
+console.log('TargetName:\t\t' + targetName + ` [ manifest: ${includeManifest} ]`);
+
+// 7z adds to existing archive - must delete old first
+if (fs.existsSync(`${targetDir}/${targetName}`)) {
+	console.log('Target Exists:\t\tRemoving old target');
+	fs.unlinkSync(`${targetDir}/${targetName}`);
+}
+
+const installRDFVersion = xml_util.rdfGetValue(`${sourceDir}/install.rdf`, 'Description[\"em:version\"]');
+const manifestVersion = loadJsonFile.sync(`${sourceDir}/manifest.json`).version;
+const manifestName = loadJsonFile.sync(`${sourceDir}/manifest.json`).name;
+const ignoreFile = (includeManifest ? null : `-x!${sourceDir}/manifest.json`);
+
+console.log('\nVersioning:\n  Target:\t\t' + targetVersion + '\n  install.rdf:\t\t' + installRDFVersion + '\n  manifest.json:\t' + manifestVersion);
+
+if (installRDFVersion !== targetVersion) {
+	console.log(`\nVersion Mismatch: [Error]\n  install.rdf: ${installRDFVersion} != package.json: ${targetVersion}`);
+	return 1;
+}
+
+if (includeManifest && manifestVersion !== targetVersion) {
+	console.log(`\nVersion Mismatch:\n  manifest.json: ${manifestVersion} != package.json: ${targetVersion}`);
+	return 1;
+}
+
+if (includeManifest && (manifestName + targetExtension) !== targetName) {
+	console.log(`\nName Mismatch:\n  manifest.json: ${manifestName} != package.json: ${targetName}`);
+	return 1;
+}
+
+let _7zCommand = ['a', `${targetDir}/${targetName}`, `${sourceDir}/*`, `-x@./src/.tb-hybrid-ignore`];
+
+if (ignoreFile) {
+	_7zCommand.push(`${ignoreFile}`);
+}
 
 // Create xpi archive using exclude file
-_7z.cmd(['a', `${targetDir}/${targetName}`, `${sourceDir}/*`, '-x@./src/.tb-hybrid-ignore'], err => {
-    // done
-	console.log('Archive done: '+err);
+_7z.cmd(_7zCommand, err => {
+	if (err) {
+		console.log('\nArchive Error: ' + err);
+		return 1;
+	}
+	console.log('\nArchive Complete: ' + targetName + ` [ manifest: ${includeManifest} ]`);
 
 });
-
-
-function handleError(err) {
-	console.error('Archive Error: '+err);
-}
--- scripts/xml-util.js
+++ scripts/xml-util.js
@@ -1,19 +1,14 @@
-var convert = require('xml-js');
-var xml = require('fs').readFileSync('./src/install.rdf', 'utf8');
-var options = {ignoreComment: true, alwaysChildren: true, compact: true};
-var result = convert.xml2js(xml, options); // or convert.xml2json(xml, options)
-console.log("start");
+// cleidigh - Utility to get install.rdf values
 
-if (process.argv[2] === '-get' && process.argv[3]) {
-	var p = process.argv[3];
-	// console.log(p);
-	p = p.replace(/^"|"$/g, '');
-	// console.log(p);
-	var rdfXMLPath = 'result.RDF.' + process.argv[3] + '._text';
-	// console.log(rdfXMLPath);
-	console.log(eval(rdfXMLPath));
-} else {
-	console.log(result.RDF.Description["em:id"]._text);
+exports.rdfGetValue = function(file, valuePath) {
+
+	var convert = require('xml-js');
+	var xml = require('fs').readFileSync(file, 'utf8');
+	var options = { ignoreComment: true, alwaysChildren: true, compact: true };
+	var result = convert.xml2js(xml, options); 
+
+	var rdfXMLPath = 'result.RDF.' + valuePath + '._text';
+	// console.log(eval(rdfXMLPath));
+
+	return eval(rdfXMLPath);
 }
-
-

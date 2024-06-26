44873e3eed0119cc2961de28077ea2c9369591cc
mic <m@micz.it>
2024-06-26 21:30:55 +0200

    options license tab updated. see #228

--- src/components/options_tabs/OPTAB_License.vue
+++ src/components/options_tabs/OPTAB_License.vue
@@ -22,44 +22,25 @@
     <OptionDonationHeader />
     <div>
       <h1>__MSG_License__</h1>
-      <p>ThunderStats is licensed under a <a href="http://creativecommons.org/licenses/by-nc/4.0/">Creative Commons Attribution-NonCommercial 4.0 International License</a>.</p>
-<p>To view a copy of this license, visit <a href="http://creativecommons.org/licenses/by-nc/4.0/">http://creativecommons.org/licenses/by-nc/4.0/</a>.</p>
+      <p>ThunderStats [https://micz.it/thunderbird-addon-thunderstats-your-thunderbird-statistics/]<br>
+   Copyright (C) 2024  Mic (m@micz.it)<br>
+   <br>
+   This program is free software: you can redistribute it and/or modify<br>
+   it under the terms of the GNU General Public License as published by<br>
+   the Free Software Foundation, either version 3 of the License, or<br>
+   (at your option) any later version.<br>
+   <br>
+   This program is distributed in the hope that it will be useful,<br>
+  but WITHOUT ANY WARRANTY; without even the implied warranty of<br>
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the<br>
+   GNU General Public License for more details.<br>
+   <br>
+   You should have received a copy of the GNU General Public License<br>
+   along with this program.  If not, see <a href="http://www.gnu.org/licenses/">http://www.gnu.org/licenses/</a>.</p>
 
-<h2>Terms:</h2>
-<p><strong>The Program</strong> - The ThunderStats Add-on (distributed as a XPI file) and all files contained within it.</p>
-<p>All contents of The Program are subject to the GNU General Public License Version 3 (the "License"); you may not use the file except in compliance with the License.</p>
-<p>If you plan to use large portions of the code please contact the author Mic at <a href="mailto:m@micz.it">m@micz.it</a>.</p>
-<p>This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the Creative Commons License for more details.</p>
-<p><strong>Addon home page:</strong> <a href="http://micz.it/thunderbird-addon-thunderstats-your-thunderbird-statistics/">http://micz.it/thunderbird-addon-thunderstats-your-thunderbird-statistics/</a></p>
+<h1>Attributions</h1>
 
-<h1>ATTRIBUTIONS</h1>
-
-<h2>Translations</h2>
-<p>(Thanks to BabelZilla.org)</p>
-<ul>
-    <li>German (de) thanks to RealRaven [<a href="http://quickfolders.mozdev.org/">http://quickfolders.mozdev.org/</a>]</li>
-    <li>Italian (it) thanks to Mic [<a href="http://micz.it">http://micz.it</a>]</li>
-    <li>French (fr) thanks to Goofy</li>
-</ul>
-
-<h2>Thanks to...</h2>
-<ul>
-    <li>Christopher Leidigh, for working hard on the Thunderbird 60+ compatibility.</li>
-    <li>Axel Grude, developer of QuickFolders addon, for his help in testing and brainstorming new features.</li>
-    <li>Jörg Knobloch, Thunderbird contributor, for his help in fixing this addon for Thunderbird 60.</li>
-</ul>
-
-<h2>Javascript Libraries</h2>
-<ul>
-    <li>Chartjs - <a href="https://www.chartjs.org/">https://www.chartjs.org/</a><br>
-    Provided under the MIT license: <a href="https://github.com/chartjs/Chart.js?tab=MIT-1-ov-file">https://github.com/chartjs/Chart.js?tab=MIT-1-ov-file</a></li>
-</ul>
-
-<h2>Graphics</h2>
-<ul>
-    <li>Addon icon thanks to <a href="http://hopstarter.deviantart.com/">http://hopstarter.deviantart.com/</a></li>
-    <li>Custom Query Bookmark Icon thanks to <a href="http://www.iconarchive.com/artist/graphicrating.html">http://www.iconarchive.com/artist/graphicrating.html</a></li>
-</ul>
+See <a href="https://github.com/micz/ThunderStats/blob/new_2.0/README.md#attributions">https://github.com/micz/ThunderStats/blob/new_2.0/README.md#attributions</a>
     </div>
 </template>
   

b531da5e0353c1d85316b1fcd4f36c066e40fdcd
mic <m@micz.it>
2024-07-18 21:54:08 +0200

    release notes updated

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -4,7 +4,17 @@
 - ...
 
 
-<i>This is a complete rewrite of the original addon as a MailExtension, using Vue.js.</i>
+<h3>Version 2.0.0pre1 - ??/??/2024</h3>
+      <ul>
+       <li>This is a complete rewrite of the original addon as a MailExtension, using Vue.js.</li>
+       <li>Added dark mode [<a href="https://github.com/micz/ThunderStats/issues/197">#197</a>].</li>
+       <li>It's now possibile to define a locale for the datepicker different from the default one [<a href="https://github.com/micz/ThunderStats/issues/238">#238</a>].</li>
+       <li>Added an option to choose to remember last opened tab [<a href="https://github.com/micz/ThunderStats/issues/232">#232</a>].</li>
+       <li>The Time Graph is now a line graph [<a href="https://github.com/micz/ThunderStats/issues/198">#198</a>].</li>
+       <li>Added info for the last days in the Yesterday Tab [<a href="https://github.com/micz/ThunderStats/issues/276">#276</a>].</li>
+       <li>The elaboration time is now shown [<a href="https://github.com/micz/ThunderStats/issues/218">#218</a>].</li>
+       <li>The results are displayed as they become available [<a href="https://github.com/micz/ThunderStats/issues/219">#219</a>].</li>
+      </ul>
 
 <br><br><br>
 
--- src/components/options_tabs/OPTAB_Info.vue
+++ src/components/options_tabs/OPTAB_Info.vue
@@ -24,9 +24,15 @@
       <h1>ThunderStats Release Notes</h1>
       <h3>Version 2.0.0pre1 - ??/??/2024</h3>
       <ul>
-       <li>...</li>
+       <li>This is a complete rewrite of the original addon as a MailExtension, using Vue.js.</li>
+       <li>Added dark mode [<a href="https://github.com/micz/ThunderStats/issues/197">#197</a>].</li>
+       <li>It's now possibile to define a locale for the datepicker different from the default one [<a href="https://github.com/micz/ThunderStats/issues/238">#238</a>].</li>
+       <li>Added an option to choose to remember last opened tab [<a href="https://github.com/micz/ThunderStats/issues/232">#232</a>].</li>
+       <li>The Time Graph is now a line graph [<a href="https://github.com/micz/ThunderStats/issues/198">#198</a>].</li>
+       <li>Added info for the last days in the Yesterday Tab [<a href="https://github.com/micz/ThunderStats/issues/276">#276</a>].</li>
+       <li>The elaboration time is now shown [<a href="https://github.com/micz/ThunderStats/issues/218">#218</a>].</li>
+       <li>The results are displayed as they become available [<a href="https://github.com/micz/ThunderStats/issues/219">#219</a>].</li>
       </ul>
-      <p>This is a complete rewrite of the original addon as a MailExtension, using Vue.js.</p>
     </div>
     <div v-if="!show_v1"><button type="button" @click="show_v1 = true">__MSG_ShowRelNotes1x__</button></div>
     <div v-if="show_v1">

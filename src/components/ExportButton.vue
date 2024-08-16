eaa1c6539dc2e23b8e5d41dfaef756ff051c8eb5
mic <m@micz.it>
2024-08-16 23:06:32 +0200

    added new props additional_css_class and link_text is now a prop

--- src/components/ExportButton.vue
+++ src/components/ExportButton.vue
@@ -1,5 +1,5 @@
 <template>
-<div @click="exportData(export_data, export_name)" class="export_data">{{ link_text }}</div>
+<div @click="exportData(export_data, export_name)" :class="css_class">{{ link_text }}</div>
 </template>
 
 
@@ -21,18 +21,23 @@ const props = defineProps({
   export_type: {
     type: String,
     default: ""
+  },
+  additional_css_class: {
+    type: String,
+    default: ""
+  },
+  link_text: {
+    type: String,
+    default: browser.i18n.getMessage("Export")
   }
 });
 
 let export_data = computed(() => props.export_data)
 let export_name = computed(() => props.export_name)
 let export_type = computed(() => props.export_type)
-
-let link_text = ref('');
-
-onMounted(() => {
-    link_text.value = browser.i18n.getMessage("Export");
-});
+let css_class = computed(() => "export_data " + props.additional_css_class)
+let additional_link_text = computed(() => props.additional_link_text)
+let link_text = computed(() => props.link_text)
 
 
 function exportData(data, export_name) {
@@ -42,6 +47,12 @@ function exportData(data, export_name) {
         case "correspondents":
             let output_data = tsExport.transformCorrespondentsJsonToArray(data);
             tsExport.downloadCSV(output_data, export_name);
+          break;
+        case 'daily_mails':
+          //console.log(">>>>>>> exportData daily_mails: " + JSON.stringify(data));
+          let output_data_daily_mails = tsExport.transformDailyMailsJsonToArray(data);
+          //console.log(">>>>>>>> output_data_daily_mails: " + JSON.stringify(output_data_daily_mails));
+          tsExport.downloadCSV(output_data_daily_mails, export_name);
           break;
         default: alert("export error: " + export_type.value);
           break;

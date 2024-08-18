f65b7f47ea77881f23fe30e662c4b260b808d716
mic <m@micz.it>
2024-08-18 15:55:02 +0200

    new ExportMenu Vue component added. see #333

---
+++ src/components/ExportMenu.vue
@@ -0,0 +1,152 @@
+<template>
+<div class="exportdata_menu" :id="exportdata_menu_dom_id">
+                <img src="@/assets/images/mzts-export-data.png" @click="openExportMenu" @contextmenu="openExportMenu" title="__MSG_ExportData__" class="exportmenu"/>
+            </div>
+</template>
+
+
+<script setup>
+
+import { ref, computed, onMounted } from 'vue';
+import { tsExport } from '@statslib/mzts-export';
+import { tsStore } from '@statslib/mzts-store';
+import { tsUtils } from '@statslib/mzts-utils';
+import ContextMenu from '@imengyu/vue3-context-menu'
+import '@imengyu/vue3-context-menu/lib/vue3-context-menu.css'
+
+
+const props = defineProps({
+  export_data: {
+    type: Object,
+    default: {}
+  },
+  currentTab: {
+    type: String,
+    default: ""
+  },
+  // additional_css_class: {
+  //   type: String,
+  //   default: ""
+  // },
+});
+
+let export_data = computed(() => props.export_data)
+let currentTab = computed(() => props.currentTab)
+// let css_class = computed(() => "export_data " + props.additional_css_class)
+
+let exportdata_menu_dom_id = ref("exportdata_menu_" + currentTab.value);
+
+
+let export_menu = {
+    "tab-today": [     //========================================== Today Tab
+        { 
+          label: browser.i18n.getMessage("Correspondents"), 
+          onClick: () => {
+            let export_define = browser.i18n.getMessage("Day") + tsUtils.dateToYYYYMMDD(new Date());
+            let export_type = tsExport.export["correspondents"].type;
+            exportData(export_data.value[export_type], export_type, export_define + "_" + tsExport.export[export_type].name)
+          }
+        },
+        { 
+          label: browser.i18n.getMessage("MailsInADay"), 
+          onClick: () => {
+            let export_define = browser.i18n.getMessage("Day") + tsUtils.dateToYYYYMMDD(new Date());
+            let export_type = tsExport.export["time_emails"].type;
+            exportData(export_data.value[export_type], export_type, export_define + "_" + tsExport.export[export_type].name)
+          }
+        },
+        // { 
+        //   label: browser.i18n.getMessage("Last2Week"), 
+        //   onClick: () => {
+        //     setPeriod("last2week");
+        //   }
+        // },
+        // { 
+        //   label: browser.i18n.getMessage("CurrentMonth"), 
+        //   onClick: () => {
+        //     setPeriod("currentmonth");
+        //   }
+        // },
+        // { 
+        //   label: browser.i18n.getMessage("LastMonth"),
+        //   onClick: () => {
+        //     setPeriod("lastmonth");
+        //   }
+        // },
+        // { 
+        //   label: browser.i18n.getMessage("CurrentYear"),
+        //   onClick: () => {
+        //     setPeriod("currentyear");
+        //   }
+        // },
+        // { 
+        //   label: browser.i18n.getMessage("LastYear"),
+        //   onClick: () => {
+        //     setPeriod("lastyear");
+        //   }
+        // },
+        /*{
+          label: "A submenu", 
+          children: [
+            { label: "Item1" },
+            { label: "Item2" },
+            { label: "Item3" },
+          ]
+        },*/
+      ], //========================================== Today Tab - END
+};
+
+
+function openExportMenu(e){
+    e.preventDefault();
+    let menu_icon = document.getElementById(exportdata_menu_dom_id.value).getBoundingClientRect();
+    let x = menu_icon.x + menu_icon.width/2;
+    let y = menu_icon.y + menu_icon.height/2;
+    ContextMenu.showContextMenu({
+      theme: tsStore.darkmode ? "mac dark" : "mac",
+      x: x,
+      y: y,
+      items: export_menu[currentTab.value],
+  });
+}
+
+
+function exportData(data, export_type, export_name) {
+    // console.log(">>>>>>>>>>>>> exportData data: " + JSON.stringify(data));
+
+    switch(export_type) {
+        case "correspondents":
+            let output_data = tsExport.transformCorrespondentsJsonToArray(data);
+            tsExport.downloadCSV(output_data, export_name);
+          break;
+        case 'daily_mails':
+          //console.log(">>>>>>> exportData daily_mails: " + JSON.stringify(data));
+          let output_data_daily_mails = tsExport.transformDailyMailsJsonToArray(data);
+          //console.log(">>>>>>>> output_data_daily_mails: " + JSON.stringify(output_data_daily_mails));
+          tsExport.downloadCSV(output_data_daily_mails, export_name);
+          break;
+        case "time_emails":
+          let output_data_time_emails = tsExport.transformTimeMailsJsonToArray(data);
+          tsExport.downloadCSV(output_data_time_emails, export_name);
+          break;
+        default: alert("export error: " + export_type);
+          break;
+    }
+}
+
+</script>
+
+
+<style scoped>
+.exportdata_menu{
+  position: absolute;
+  right: 5px;
+  top: -0.5rem;
+  z-index: 1500;
+}
+.exportmenu{
+  height: 24px;
+  width: 24px;
+  cursor: pointer;
+}
+</style>

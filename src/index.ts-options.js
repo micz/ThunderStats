1d0e144bc25330e2e66f2b8d2f7d2365b25232b8
mic <m@micz.it>
2024-10-05 22:41:21 +0200

    inbox0_openFolderInFirstTab options is now again visible in TB128

--- src/index.ts-options.js
+++ src/index.ts-options.js
@@ -6,10 +6,12 @@ import {Tabs, Tab} from 'vue3-tabs-component';
 import App_Options from './App_Options.vue';
 import { tsStore } from '@statslib/mzts-store.js';
 import { tsPrefs } from '@statslib/mzts-options';
+import { tsUtils } from '@statslib/mzts-utils';
 
 async function main() {
 
     tsStore.do_debug = await tsPrefs.getPref("do_debug");
+    tsStore.isTB128plus = await tsUtils.isThunderbird128OrGreater();
     tsStore.darkmode = undefined;
 
     createApp(App_Options)

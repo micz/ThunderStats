f89da1e9dcc46c9d04b71993c508d61166daf5a8
mic <m@micz.it>
2024-11-23 22:06:00 +0100

    indentation fixed

--- src/components/ExportMenu.vue
+++ src/components/ExportMenu.vue
@@ -73,134 +73,96 @@ let export_menu = {
             exportData(export_data.value[export_type], export_type, export_define + "_" + tsExport.export[export_type].name)
           }
         },
-        // { 
-        //   label: browser.i18n.getMessage("Last2Week"), 
-        //   onClick: () => {
-        //     setPeriod("last2week");
-        //   }
-        // },
-        // { 
-        //   label: browser.i18n.getMessage("CurrentMonth"), 
-        //   onClick: () => {
-        //     setPeriod("currentmonth");
-        //   }
-        // },
-        // { 
-        //   label: browser.i18n.getMessage("LastMonth"),
-        //   onClick: () => {
-        //     setPeriod("lastmonth");
-        //   }
-        // },
-        // { 
-        //   label: browser.i18n.getMessage("CurrentYear"),
-        //   onClick: () => {
-        //     setPeriod("currentyear");
-        //   }
-        // },
-        // { 
-        //   label: browser.i18n.getMessage("LastYear"),
-        //   onClick: () => {
-        //     setPeriod("lastyear");
-        //   }
-        // },
-        /*{
-          label: "A submenu", 
-          children: [
-            { label: "Item1" },
-            { label: "Item2" },
-            { label: "Item3" },
-          ]
-        },*/
-      ],
-      //========================================== Today Tab - END
-      //========================================== Yesterday Tab
-      "tab-yesterday": [
-        { 
-          label: browser.i18n.getMessage("Correspondents"), 
-          onClick: () => {
-            let export_define = browser.i18n.getMessage("Day") + tsUtils.dateToYYYYMMDD(new Date(Date.now() - (1000 * 60 * 60 * 24)));
-            let export_type = tsExport.export["correspondents"].type;
+    ],
+    //========================================== Today Tab - END
+    //========================================== Yesterday Tab
+    "tab-yesterday": [
+      { 
+        label: browser.i18n.getMessage("Correspondents"), 
+        onClick: () => {
+          let export_define = browser.i18n.getMessage("Day") + tsUtils.dateToYYYYMMDD(new Date(Date.now() - (1000 * 60 * 60 * 24)));
+          let export_type = tsExport.export["correspondents"].type;
+          exportData(export_data.value[export_type], export_type, export_define + "_" + tsExport.export[export_type].name)
+        }
+      },
+      { 
+        label: browser.i18n.getMessage("MailsInADay"), 
+        onClick: () => {
+          let export_define = browser.i18n.getMessage("Day") + tsUtils.dateToYYYYMMDD(new Date(Date.now() - (1000 * 60 * 60 * 24)));
+          let export_type = tsExport.export["time_emails"].type;
             exportData(export_data.value[export_type], export_type, export_define + "_" + tsExport.export[export_type].name)
           }
-        },
-        { 
-          label: browser.i18n.getMessage("MailsInADay"), 
-          onClick: () => {
-            let export_define = browser.i18n.getMessage("Day") + tsUtils.dateToYYYYMMDD(new Date(Date.now() - (1000 * 60 * 60 * 24)));
-            let export_type = tsExport.export["time_emails"].type;
-            exportData(export_data.value[export_type], export_type, export_define + "_" + tsExport.export[export_type].name)
-          }
-        },
-      ],
-      //========================================== ManyDays Tab - END
-      "tab-manydays": [
-        { 
-          label: browser.i18n.getMessage("Correspondents"), 
-          onClick: async () => {
-            let _many_days = await tsPrefs.getPref("_many_days");
-            let export_define = browser.i18n.getMessage("LastNumDays", _many_days);
-            let export_type = tsExport.export["correspondents"].type;
-            exportData(export_data.value[export_type], export_type, export_define + "_" + tsExport.export[export_type].name)
-          }
-        },
-        { 
-          label: browser.i18n.getMessage("DailyMails"), 
-          onClick: async () => {
-            let _many_days = await tsPrefs.getPref("_many_days");
-            let export_define = browser.i18n.getMessage("LastNumDays", _many_days);
-            let export_type = tsExport.export["daily_mails"].type;
-            exportData(export_data.value[export_type], export_type, export_define + "_" + tsExport.export[export_type].name)
-          }
-        },
-        // { 
-        //   label: browser.i18n.getMessage("MailsInADay"), 
-        //   onClick: async () => {
-        //     let _many_days = await tsPrefs.getPref("_many_days");
-        //     let export_define = browser.i18n.getMessage("LastNumDays", _many_days);
-        //     let export_type = tsExport.export["time_emails"].type;
-        //     exportData(export_data.value[export_type], export_type, export_define + "_" + tsExport.export[export_type].name)
-        //   }
-        // },
-      ],
-      //========================================== ManyDays Tab - END
-      //========================================== CustomQry Tab - END
-      "tab-customqry": [
-        { 
-          label: browser.i18n.getMessage("Correspondents"), 
-          onClick: () => {
-            let export_define = browser.i18n.getMessage("CustomQry");
-            let export_type = tsExport.export["correspondents"].type;
-            exportData(export_data.value[export_type], export_type, export_define + "_" + tsExport.export[export_type].name)
-          }
-        },
-        { 
-          label: browser.i18n.getMessage("DailyMails"), 
-          onClick: () => {
-            let export_define = browser.i18n.getMessage("CustomQry");
-            let export_type = tsExport.export["daily_mails"].type;
-            exportData(export_data.value[export_type], export_type, export_define + "_" + tsExport.export[export_type].name)
-          }
-        },
-      ],
-      "tab-customqry-single-day": [
-        { 
-          label: browser.i18n.getMessage("Correspondents"), 
-          onClick: () => {
-            let export_define = browser.i18n.getMessage("CustomQry");
-            let export_type = tsExport.export["correspondents"].type;
-            exportData(export_data.value[export_type], export_type, export_define + "_" + tsExport.export[export_type].name)
-          }
-        },
-        { 
-          label: browser.i18n.getMessage("MailsInADay"), 
-          onClick: async () => {
-            let export_define = browser.i18n.getMessage("Day") + tsUtils.dateToYYYYMMDD(singleDay.value);
-            let export_type = tsExport.export["time_emails"].type;
-            exportData(export_data.value[export_type], export_type, export_define + "_" + tsExport.export[export_type].name)
-          }
-        },
-      ],
-      //========================================== CustomQry Tab - END
+      },
+    ],
+    //========================================== ManyDays Tab - END
+    "tab-manydays": [
+      { 
+        label: browser.i18n.getMessage("Correspondents"), 
+        onClick: async () => {
+          let _many_days = await tsPrefs.getPref("_many_days");
+          let export_define = browser.i18n.getMessage("LastNumDays", _many_days);
+          let export_type = tsExport.export["correspondents"].type;
+          exportData(export_data.value[export_type], export_type, export_define + "_" + tsExport.export[export_type].name)
+        }
+      },
+      { 
+        label: browser.i18n.getMessage("DailyMails"), 
+        onClick: async () => {
+          let _many_days = await tsPrefs.getPref("_many_days");
+          let export_define = browser.i18n.getMessage("LastNumDays", _many_days);
+          let export_type = tsExport.export["daily_mails"].type;
+          exportData(export_data.value[export_type], export_type, export_define + "_" + tsExport.export[export_type].name)
+        }
+      },
+      // { 
+      //   label: browser.i18n.getMessage("MailsInADay"), 
+      //   onClick: async () => {
+      //     let _many_days = await tsPrefs.getPref("_many_days");
+      //     let export_define = browser.i18n.getMessage("LastNumDays", _many_days);
+      //     let export_type = tsExport.export["time_emails"].type;
+      //     exportData(export_data.value[export_type], export_type, export_define + "_" + tsExport.export[export_type].name)
+      //   }
+      // },
+    ],
+    //========================================== ManyDays Tab - END
+    //========================================== CustomQry Tab - END
+    "tab-customqry": [
+      { 
+        label: browser.i18n.getMessage("Correspondents"), 
+        onClick: () => {
+          let export_define = browser.i18n.getMessage("CustomQry");
+          let export_type = tsExport.export["correspondents"].type;
+          exportData(export_data.value[export_type], export_type, export_define + "_" + tsExport.export[export_type].name)
+        }
+      },
+      { 
+        label: browser.i18n.getMessage("DailyMails"), 
+        onClick: () => {
+          let export_define = browser.i18n.getMessage("CustomQry");
+          let export_type = tsExport.export["daily_mails"].type;
+          exportData(export_data.value[export_type], export_type, export_define + "_" + tsExport.export[export_type].name)
+        }
+      },
+    ],
+    "tab-customqry-single-day": [
+      { 
+        label: browser.i18n.getMessage("Correspondents"), 
+        onClick: () => {
+          let export_define = browser.i18n.getMessage("CustomQry");
+          let export_type = tsExport.export["correspondents"].type;
+          exportData(export_data.value[export_type], export_type, export_define + "_" + tsExport.export[export_type].name)
+        }
+      },
+      { 
+        label: browser.i18n.getMessage("MailsInADay"), 
+        onClick: async () => {
+          let export_define = browser.i18n.getMessage("Day") + tsUtils.dateToYYYYMMDD(singleDay.value);
+          let export_type = tsExport.export["time_emails"].type;
+          exportData(export_data.value[export_type], export_type, export_define + "_" + tsExport.export[export_type].name)
+        }
+      },
+    ],
+    //========================================== CustomQry Tab - END
 };
 
 

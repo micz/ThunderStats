9fac6972ace38b844342e6c157bfb0b640c29332
Mic <m@micz.it>
2024-11-25 00:19:00 +0100

    folders export added to customqry tab. see #394

--- src/components/ExportMenu.vue
+++ src/components/ExportMenu.vue
@@ -194,9 +194,17 @@ let export_menu = {
       },
       { 
         label: browser.i18n.getMessage("MailsInADay"), 
-        onClick: async () => {
+        onClick: () => {
           let export_define = browser.i18n.getMessage("CustomQry");
           let export_type = tsExport.export["time_emails"].type;
+          exportData(export_data.value[export_type], export_type, export_define + "_" + tsExport.export[export_type].name)
+        }
+      },
+      {
+        label: browser.i18n.getMessage("Folders"),
+        onClick: () => {
+          let export_define = browser.i18n.getMessage("CustomQry");
+          let export_type = tsExport.export["folders"].type;
           exportData(export_data.value[export_type], export_type, export_define + "_" + tsExport.export[export_type].name)
         }
       },
@@ -223,6 +231,14 @@ let export_menu = {
         onClick: async () => {
           let export_define = browser.i18n.getMessage("Day") + tsUtils.dateToYYYYMMDD(singleDay.value);
           let export_type = tsExport.export["time_emails"].type;
+          exportData(export_data.value[export_type], export_type, export_define + "_" + tsExport.export[export_type].name)
+        }
+      },
+      {
+        label: browser.i18n.getMessage("Folders"),
+        onClick: () => {
+          let export_define = browser.i18n.getMessage("Day") + tsUtils.dateToYYYYMMDD(singleDay.value);
+          let export_type = tsExport.export["folders"].type;
           exportData(export_data.value[export_type], export_type, export_define + "_" + tsExport.export[export_type].name)
         }
       },

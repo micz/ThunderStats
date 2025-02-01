51dd4fcddf8b454694e5db81d589557c9c8f259e
mic <m@micz.it>
2025-02-01 21:37:22 +0100

    using messenger.spaces instead of browser.spacesToolbar. fixes #399

--- public/background.js
+++ public/background.js
@@ -21,29 +21,19 @@ browser.browserAction.onClicked.addListener(() => {
   });
 
   function openTStab() {
-    // check if the tab is already there
-    /*browser.tabs.query({url: browser.runtime.getURL("./index.thunderstats.html")}).then((tabs) => {
-      if (tabs.length > 0) {
-        // if the tab is already there, focus it
-        browser.tabs.update(tabs[0].id, {active: true});
-      } else {
-        // if the tab is not there, create it
-        browser.tabs.create({url: browser.runtime.getURL("./index.thunderstats.html")});
-      }
-    })*/
-    browser.spacesToolbar.clickButton('thunderstats');
+      messenger.spaces.open('thunderstats');
  }
 
 
-if (browser.spacesToolbar) {
-  browser.spacesToolbar.addButton(
+ if (messenger.spaces) {
+  await messenger.spaces.create(
     'thunderstats',
+    './index.thunderstats.html',
     {
       badgeBackgroundColor: '#e64db9',
       badgeText: '',
       defaultIcons: 'images/mzts-icon-16px.png',
-      title: 'ThunderStats',
-      url: './index.thunderstats.html'
+      title: 'ThunderStats'
     }
   );
 }
